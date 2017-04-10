60bf6bc7d777051198e243a387d753820a22322a
Haolun <chshawkn@hotmail.com>
2017-04-10 18:19:17 +0800

    init commit

---
+++ src/main/jira/jira-local.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# TODO 使用低权限用户, move this to install phase
+export BUILD_JIRA_PROJECTKEY=${BUILD_JIRA_PROJECTKEY}
+export BUILD_JIRA_USER=${BUILD_JIRA_USER}
+export BUILD_JIRA_PASSWORD=${BUILD_JIRA_PASSWORD}
