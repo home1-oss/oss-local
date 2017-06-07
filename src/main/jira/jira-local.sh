2dd5863f77a310a8f69278976dfcf18822620d17
Haolun <chshawkn@hotmail.com>
2017-06-07 17:56:48 +0800

    fix local config

--- src/main/jira/jira-local.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# TODO 使用低权限用户, move this to install phase
-export BUILD_JIRA_PROJECTKEY=${BUILD_JIRA_PROJECTKEY}
-export BUILD_JIRA_USER=${BUILD_JIRA_USER}
-export BUILD_JIRA_PASSWORD=${BUILD_JIRA_PASSWORD}
