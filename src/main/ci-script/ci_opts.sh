18cc5c602ce02a1df6043996ac83fb96e788f8a7
Haolun <chshawkn@hotmail.com>
2017-06-14 20:26:08 +0800

    fix ci_opts.sh

--- src/main/ci-script/ci_opts.sh
+++ src/main/ci-script/ci_opts.sh
@@ -1,5 +1,5 @@
 
 #export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=${BUILD_SCRIPT_LOC}/src/main/checkstyle/google_checks_6.19.xml"
 #export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=${BUILD_SCRIPT_LOC}/src/main/pmd/pmd-ruleset-5.3.5.xml"
-#export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.nodeDownloadRoot=http://nexus.local:28081/nexus/repository/nexus/repository/npm-dist/"
-#export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.npmDownloadRoot=http://nexus.local:28081/nexus/repository/nexus/repository/npm-public/npm/-/"
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.nodeDownloadRoot=http://nexus.local:28081/nexus/repository/npm-dist/"
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.npmDownloadRoot=http://nexus.local:28081/nexus/repository/npm-public/npm/-/"
